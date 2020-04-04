8f9a3726f04cc3e8ea51bb38a89fa798a2ef55da
Ingunn Augdal Fløvig <ingunn.flovig@gmail.com>
2020-04-04 18:26:16 +0200

    Added AIMA functions, removed other search functions

--- search.py
+++ search.py
@@ -3,7 +3,7 @@ This module contains functions and data types for the creation and searching of 
 These functions are used to find the a winning set of moves
 """
 
-
+import util as u
 
 def search(initial_board_configuration) :
     """ Takes the initial board configuration as input
@@ -27,7 +27,8 @@ def search(initial_board_configuration) :
             break
         else: 
             # generate the children of current node and add to the priorityqueue
-            
+        
+    return [] #return an action sequence that leads to a winnin state
 
 
 
@@ -39,117 +40,179 @@ def compare_depth_nodes(newitem, parent):
         return False
 
 
+
+""" This function needs to be called to update our have_won bool value each time we create a new node"""
+def have_won() :
+
+    # loops through all coordinates on the board
+    for x in range(0,8) :
+         for y in range(0,8) :
+
+            # if a black token is found on board, return False
+            if (x,y) in board : 
+                if board[(x,y)].col == "b" :
+                    return False
+     # there are no more black pieces on the board, so return True
+     return True    
+
+
+
+from collections import deque
+
+from utils import *
+
+
+class Problem:
+    """The abstract class for a formal problem. You should subclass
+    this and implement the methods actions and result, and possibly
+    __init__, goal_test, and path_cost. Then you will create instances
+    of your subclass and solve them with the various search functions."""
+
+    def __init__(self, initial, goal=None):
+        """The constructor specifies the initial state, and possibly a goal
+        state, if there is a unique goal. Your subclass's constructor can add
+        other arguments."""
+        self.initial = initial
+        self.goal = goal
+
+    def actions(self, state):
+        """Return the actions that can be executed in the given
+        state. The result would typically be a list, but if there are
+        many actions, consider yielding them one at a time in an
+        iterator, rather than building them all at once."""
+        raise NotImplementedError
+
+    def result(self, state, action):
+        """Return the state that results from executing the given
+        action in the given state. The action must be one of
+        self.actions(state)."""
+        raise NotImplementedError
+
+    def goal_test(self, state):
+        """Return True if the state is a goal. The default method compares the
+        state to self.goal or checks for state in self.goal if it is a
+        list, as specified in the constructor. Override this method if
+        checking against a single self.goal is not enough."""
+        if isinstance(self.goal, list):
+            return is_in(state, self.goal)
+        else:
+            return state == self.goal
+
+    def path_cost(self, c, state1, action, state2):
+        """Return the cost of a solution path that arrives at state2 from
+        state1 via action, assuming cost c to get up to state1. If the problem
+        is such that the path doesn't matter, this function will only look at
+        state2. If the path does matter, it will consider c and maybe state1
+        and action. The default method costs 1 for every step in the path."""
+        return c + 1
+
+    def value(self, state):
+        """For optimization problems, each state has a value. Hill Climbing
+        and related algorithms try to maximize this value."""
+        raise NotImplementedError
+
+
+# ______________________________________________________________________________
+
+
 class Node:
-    """ Represents a state of the game, connected to other nodes by Moves 
-        Contains
-        """
+    """A node in a search tree. Contains a pointer to the parent (the node
+    that this is a successor of) and to the actual state for this node. Note
+    that if a state is arrived at by two paths, then there are two nodes with
+    the same state. Also includes the action that got us to this state, and
+    the total path_cost (also known as g) to reach the node. Other functions
+    may add an f and h value; see best_first_graph_search and astar_search for
+    an explanation of how the f and h values are handled. You will not need to
+    subclass this class."""
 
-    def __init__(self, parent_node=null, move):
-        self.depth = parent.depth + 1 #depth of the node in the search tree, represents number of moves away from current state 
-        self.parent_node = parent_node
-        self.move = move
-        self.board_configuration = board
-        
-        
+    def __init__(self, state, parent=None, action=None, path_cost=0):
+        """Create a search tree Node, derived from a parent by an action."""
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.path_cost = path_cost
+        self.depth = 0
+        if parent:
+            self.depth = parent.depth + 1
 
-    """ This function needs to be called to update our have_won bool value each time we create a new node"""
-    def have_won() :
+    def __repr__(self):
+        return "<Node {}>".format(self.state)
 
-        # loops through all coordinates on the board
-        for x in range(0,8) :
-            for y in range(0,8) :
+    def __lt__(self, node):
+        return self.state < node.state
 
-                # if a black token is found on board, return False
-                if (x,y) in board : 
-                    if board[(x,y)].col == "b" :
-                        return False
+    def expand(self, problem):
+        """List the nodes reachable in one step from this node."""
+        return [self.child_node(problem, action)
+                for action in problem.actions(self.state)]
 
-        # there are no more black pieces on the board, so return True
-        return True
+    def child_node(self, problem, action):
+        """[Figure 3.10]"""
+        next_state = problem.result(self.state, action)
+        next_node = Node(next_state, self, action, problem.path_cost(self.path_cost, self.state, action, next_state))
+        return next_node
 
+    def solution(self):
+        """Return the sequence of actions to go from the root to this node."""
+        return [node.action for node in self.path()[1:]]
 
+    def path(self):
+        """Return a list of nodes forming the path from the root to this node."""
+        node, path_back = self, []
+        while node:
+            path_back.append(node)
+            node = node.parent
+        return list(reversed(path_back))
 
-class Move:
-    """ A Move serves as an edge in the search tree and stores information about the move that connects two nodes
-        n = number of tokens moved
-        a = where a token was moved from
-        b = where a token was moved to
-        boom = boolean value that stores whether or not this Move is a boom
-    """
+    # We want for a queue of nodes in breadth_first_graph_search or
+    # astar_search to have no duplicated states, so we treat nodes
+    # with the same state as equal. [Problem: this may not be what you
+    # want in other contexts.]
 
-    def __init__(self, n, a, b, boom=False) :
-        self.n = n
-        self.a = a
-        self.b = b
-        self.boom = boom
-        pass
+    def __eq__(self, other):
+        return isinstance(other, Node) and self.state == other.state
 
-
-
-class PriorityQueue: 
-
-    def __init__(self) :
-        self.priorityqueue = []
-
-    #method borrowed from https://github.com/python/cpython/blob/ba8a2bcebfdb41acafea9a195e45e9d177dc216f/Lib/heapq.py#L140
-    def heappush(self, node) :
-        """ Push item onto heap, maintain the heap invariant """
-        heap.append(node)
-        _siftdown(heap, 0, len(heap)-1)
-
-    #method borrowed from https://github.com/python/cpython/blob/ba8a2bcebfdb41acafea9a195e45e9d177dc216f/Lib/heapq.py#L140
-    """ 'heap' is a heap at all indices >= startpos, except possibly for pos.  pos
-    # is the index of a leaf with a possibly out-of-order value.  Restore the
-    # heap invariant."""
-    def _siftdown(self, startpos, pos):
-        newitem = self.priorityqueue[pos]
-        # Follow the path to the root, moving parents down until finding a place
-        # newitem fits.
-        while pos > startpos:
-            parentpos = (pos - 1) >> 1
-            parent = self.priorityqueue[parentpos]
-            if compare_depth_nodes(newitem, parent): #if newitem has greater depth than parent, continue to sift through heap
-                self.priorityqueue[pos] = parent
-                pos = parentpos
-                continue
-            break
-        self.priorityqueue[pos] = newitem
-            
-    
-    def get_length(self) :
-        return len(self.priorityqueue)
-
-    #modified method from https://github.com/python/cpython/blob/ba8a2bcebfdb41acafea9a195e45e9d177dc216f/Lib/heapq.py#L140
-    def heappop(self) :
-        """ Returns the next node with smallest depth, maintains the heap """
-        lastelt = self.priorityqueue.pop()
-        if self.priorityqueue:
-            returnitem = self.priorityqueue[0]
-            self.priorityqueue[0] = lastelt
-            _siftup(self.priorityqueue, 0)
-        else: 
-            returnitem = lastelt
-        return returnitem
+    def __hash__(self):
+        # We use the hash value of the state
+        # stored in the node instead of the node
+        # object itself to quickly search a node
+        # with the same state in a Hash Table
+        return hash(self.state)
 
 
-    #modified method from https://github.com/python/cpython/blob/ba8a2bcebfdb41acafea9a195e45e9d177dc216f/Lib/heapq.py#L140
-    def _siftup(self, pos):
-        endpos = len(self.priorityqueue)
-        startpos = pos
-        newitem = self.priorityqueue[pos]
-        # Bubble up the smaller child until hitting a leaf.
-        childpos = 2*pos + 1    # leftmost child position
-        while childpos < endpos:
-            # Set childpos to index of smaller child.
-            rightpos = childpos + 1
-            if rightpos < endpos and not compare_depth_nodes(self.priorityqueue[childpos], self.priorityqueue[rightpos]):
-                childpos = rightpos
-            # Move the smaller child up.
-            self.priorityqueue[pos] = self.priorityqueue[childpos]
-            pos = childpos
-            childpos = 2*pos + 1
-        # The leaf at pos is empty now.  Put newitem there, and bubble it up
-        # to its final resting place (by sifting its parents down).
-        self.priorityqueue[pos] = newitem
-        _siftdown(self.priorityqueue, startpos, pos)
+# ______________________________________________________________________________
 
+def recursive_best_first_search(problem, h=None):
+    """[Figure 3.26]"""
+    h = memoize(h or problem.h, 'h')
+
+    def RBFS(problem, node, flimit):
+        if problem.goal_test(node.state):
+            return node, 0  # (The second value is immaterial)
+        successors = node.expand(problem)
+        if len(successors) == 0:
+            return None, np.inf
+        for s in successors:
+            s.f = max(s.path_cost + h(s), node.f)
+        while True:
+            # Order by lowest f value
+            successors.sort(key=lambda x: x.f)
+            best = successors[0]
+            if best.f > flimit:
+                return None, best.f
+            if len(successors) > 1:
+                alternative = successors[1].f
+            else:
+                alternative = np.inf
+            result, best.f = RBFS(problem, best, min(flimit, alternative))
+            if result is not None:
+                return result, best.f
+
+    node = Node(problem.initial)
+    node.f = h(node)
+    result, bestf = RBFS(problem, node, np.inf)
+    return result
+
+
+
+# ______________________________________________________________________________
