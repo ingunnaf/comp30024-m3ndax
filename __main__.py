50814912d0d4c15484261ba7e8c8bc2485577466
Ingunn Augdal Fløvig <ingunn.flovig@gmail.com>
2020-04-11 13:38:45 +0200

    finds solution node, but still can't find the path to the solution node, might be issues with changing the states of parent nodes while generating new nodes

--- __main__.py
+++ __main__.py
@@ -17,12 +17,18 @@ def main():
     print_board(my_board)
 
     solution_node = breadth_first_tree_search(problem)
-    
-    #path_to_solution = solution_node.path()
-    #for node in path_to_solution :
-    #    action = node.action
-    #    action.print_action()
-    
+    print(solution_node.parent.__repr__())
+    print(solution_node.depth)
+    print(solution_node.repeats)
+    """
+    path_to_solution = solution_node.path()
+    if path_to_solution: 
+        for node in path_to_solution :
+            action = node.action
+            action.print_action()
+    else: 
+        print("Actually, solution was not found? ")
+    """
 
 if __name__ == '__main__':
     main()
