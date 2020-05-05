2d6844fa248ff8df8ee6defcdc325244efbd40b4
Ingunn Augdal Fløvig <ingunn.flovig@gmail.com>
2020-05-05 19:36:18 +0200

    Copied over game functions from the AIMA github as a starting point

---
+++ m3ndax/game.py
@@ -0,0 +1,103 @@
+
+
+import copy
+import itertools
+import random
+from collections import namedtuple
+
+import numpy as np
+
+from utils import vector_add
+
+GameState = namedtuple('GameState', 'to_move, utility, board, moves')
+
+
+# ______________________________________________________________________________
+
+
+def expect_minmax(state, game):
+    """
+    [Figure 5.11]
+    Return the best move for a player after dice are thrown. The game tree
+	includes chance nodes along with min and max nodes.
+	"""
+    player = game.to_move(state)
+
+    def max_value(state):
+        v = -np.inf
+        for a in game.actions(state):
+            v = max(v, chance_node(state, a))
+        return v
+
+    def min_value(state):
+        v = np.inf
+        for a in game.actions(state):
+            v = min(v, chance_node(state, a))
+        return v
+
+    def chance_node(state, action):
+        res_state = game.result(state, action)
+        if game.terminal_test(res_state):
+            return game.utility(res_state, player)
+        sum_chances = 0
+        num_chances = len(game.chances(res_state))
+        for chance in game.chances(res_state):
+            res_state = game.outcome(res_state, chance)
+            util = 0
+            if res_state.to_move == player:
+                util = max_value(res_state)
+            else:
+                util = min_value(res_state)
+            sum_chances += util * game.probability(chance)
+        return sum_chances / num_chances
+
+    # Body of expect_minmax:
+    return max(game.actions(state), key=lambda a: chance_node(state, a), default=None)
+
+#AIMA class, examples of how to implement it on https://github.com/aimacode/aima-python/blob/master/games.py
+class Game:
+    """A game is similar to a problem, but it has a utility for each
+    state and a terminal test instead of a path cost and a goal
+    test. To create a game, subclass this class and implement actions,
+    result, utility, and terminal_test. You may override display and
+    successors or you can inherit their default methods. You will also
+    need to set the .initial attribute to the initial state; this can
+    be done in the constructor."""
+
+    def actions(self, state):
+        """Return a list of the allowable moves at this point."""
+        raise NotImplementedError
+
+    def result(self, state, move):
+        """Return the state that results from making a move from a state."""
+        raise NotImplementedError
+
+    def utility(self, state, player):
+        """Return the value of this final state to player."""
+        raise NotImplementedError
+
+    def terminal_test(self, state):
+        """Return True if this is a final state for the game."""
+        return not self.actions(state)
+
+    def to_move(self, state):
+        """Return the player whose move it is in this state."""
+        return state.to_move
+
+    def display(self, state):
+        """Print or otherwise display the state."""
+        print(state)
+
+    def __repr__(self):
+        return '<{}>'.format(self.__class__.__name__)
+
+    def play_game(self, *players):
+        """Play an n-person, move-alternating game."""
+        state = self.initial
+        while True:
+            for player in players:
+                move = player(self, state)
+                state = self.result(state, move)
+                if self.terminal_test(state):
+                    self.display(state)
+                    return self.utility(state, self.to_move(self.initial))
