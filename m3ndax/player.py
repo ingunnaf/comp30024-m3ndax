ba1878b118c4df147a18e2f809059bb759cb57e7
Tate Deklerk <tdeklerk@student.unimelb.edu.au>
2020-05-06 19:25:00 +1000

    added util.py to store any methods or classes we might need.

--- m3ndax/player.py
+++ m3ndax/player.py
@@ -48,9 +48,9 @@ class ExamplePlayer:
         # Placeholder: just returns a valid action
         # Consults Expendibots class to determine what valid actions are?
 
-        #for testing purposes to see that init and update methods work as intended, 
+        #for testing purposes to see that init and update methods work as intended,
         # this for loop looks for a token in our colour and says to boom this token
-        for square in self.board.keys(): 
+        for square in self.board.keys():
             if self.board[square].col == self.colour :
                 return ("BOOM", square)
 
@@ -79,7 +79,7 @@ class ExamplePlayer:
         if action_type == "BOOM": # action is a BOOM
             origin = action[1]
             self.board = boom(origin, self.board)
-        
+
         else: # action is a MOVE
             n = action[1]
             loc_a = action[2]
@@ -91,60 +91,6 @@ class ExamplePlayer:
         # TODO: Update state representation in response to action.
 
 
-def create_board(black_start_squares, white_start_squares) : 
-    board = dict()
-    for xy in black_start_squares: 
-        board[xy] = Piece(BLACK, 1)
-    for xy in white_start_squares:
-        board[xy] = Piece(WHITE, 1)
-    return board
-
-
-def boom(origin, my_board):
-
-    x, y = origin[0], origin[1]
-    my_range = 1
-
-    del my_board[origin]
-
-    right_limit = x + my_range + 1
-    left_limit = x - my_range
-    up_limit = y + my_range + 1
-    down_limit = y - my_range
-
-    for i in range(left_limit, right_limit):
-        for j in range(down_limit, up_limit):
-            if (i, j) in my_board:
-                boom((i,j), my_board)
-
-    return my_board
-
-def move_token(n, a, b, board):
-        ret_board = copy.deepcopy(board)
-        # check if move is valid
-        """if not valid_move(n, a, b, board):
-            return board""" #i commented this out because at the moment, we don't need to validate 
-            #the action given to us for update method in player class
-
-        # handle case where there is already a token at loc b (stack new tokens on top)
-        if b in ret_board:
-            current_height_b = ret_board[b].h
-            new_height_b = current_height_b + n
-            ret_board[b] = Piece("w", new_height_b)
-        else:  # loc b has no tokens yet so we can just put our new tokens there
-            ret_board[b] = Piece("w", n)
-
-        # handle potential remaining tokens at loc a
-        current_height_a = ret_board[a].h
-        new_height_a = current_height_a - n
-        if new_height_a == 0:
-            # no more tokens left at loc a
-            del ret_board[a]
-        else:
-            ret_board[a] = Piece("w", new_height_a)
-
-        # done
-        return ret_board
 
 
 # everything above this line is used by the player class at the moment, but this should probably be moved to game.py file
